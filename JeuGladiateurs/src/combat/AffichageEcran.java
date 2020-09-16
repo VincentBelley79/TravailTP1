7e0a1baa9096a80148cfeecd43a56626fe11c3e7
Patrice-Bonneau-CegepStFe <pbonneau@cegepstfe.ca>
2020-09-16 15:32:09 -0400

    Version finale. Base pour le travail pratique des étudiants. Devrait être la dernière version finale. :-)

--- JeuGladiateurs/src/combat/AffichageEcran.java
+++ JeuGladiateurs/src/combat/AffichageEcran.java
@@ -8,6 +8,10 @@ public class AffichageEcran {
     // *************************************************************************
     // *************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Méthodes d'affichage">
+    public void afficherDebutCombat() {
+        // TODO : Afficher le message du début du combat, tel que montré dans l'énoncé
+    }
+
     public void afficherSeparateurDeTour() {
         // TODO : Afficher un retour à la ligne, suivi d'une ligne d'astérisques, tel que montré dans l'énoncé
     }
